134bac6ff791c12615b19586e4512a65834c6cdc
parag1627 <paragpatil973047@gmail.com>
2023-02-19 10:14:29 +0530

    lets see

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,7 +1,4 @@
-
 package com.driver;
-
-import java.sql.SQLOutput;
 
 public class Email {
 
@@ -28,41 +25,94 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-          if(!password.equals(oldPassword)){
-              System.out.print("Old Password is Incorrect");
-          }
-      if(newPassword.length()<8){
-          System.out.println("New Password Must Contain at Least 8 Characters");
-      }
-      boolean hasUppercase = false;
-      boolean hasLowercase = false;
-      boolean hasDigit= false;
-      boolean hasSpecialchar = false;
-      for(int i=0;i<newPassword.length();i++){
-          char ch = newPassword.charAt(i);
-          if(Character.isUpperCase(ch)){
-              hasUppercase= true;
-          }
-        else if(Character.isLowerCase(ch)){
-              hasLowercase= true;
-          }
-          else if(Character.isDigit(ch)){
-              hasDigit= true;
-          }
-          else{
-              hasSpecialchar =true;
-          }
-      }
-
-       if(!hasUppercase || !hasLowercase || !hasDigit || !hasSpecialchar){
-           System.out.println("New password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.");
-            return;
-       }
-       password= newPassword;
-      System.out.println("Password changed successfully.");
-
+        int upper=0, lower=0, special = 0, number=0;
+        int character = newPassword.length();
+        if(this.password.equals(oldPassword))
+        {
+            for(int i=0;i<newPassword.length();i++)
+            {
+                char ch = newPassword.charAt(i);
+                if(ch >= 'A' && ch<='Z') upper++;
+                else if(ch>='a' && ch<='z') lower++;
+                else if(ch>='0' && ch<= '9') number++;
+                else special++;
+            }
+            if(character>=8 && upper >= 1 && lower >=1 && number >=1 && special >= 1)
+            {
+                this.password = newPassword;
+            }
+        }
     }
 }
+
+
+
+
+
+//package com.driver;
+//
+//import java.sql.SQLOutput;
+
+//public class Email {
+//
+//    private String emailId;
+//    private String password;
+//
+//    public Email(String emailId){
+//        this.emailId = emailId;
+//        this.password = "Accio@123";
+//    }
+//
+//    public String getEmailId() {
+//        return emailId;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void changePassword(String oldPassword, String newPassword){
+//        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+//        // 1. It contains at least 8 characters
+//        // 2. It contains at least one uppercase letter
+//        // 3. It contains at least one lowercase letter
+//        // 4. It contains at least one digit
+//        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+//          if(!password.equals(oldPassword)){
+//              System.out.print("Old Password is Incorrect");
+//          }
+//      if(newPassword.length()<8){
+//          System.out.println("New Password Must Contain at Least 8 Characters");
+//      }
+//      boolean hasUppercase = false;
+//      boolean hasLowercase = false;
+//      boolean hasDigit= false;
+//      boolean hasSpecialchar = false;
+//      for(int i=0;i<newPassword.length();i++){
+//          char ch = newPassword.charAt(i);
+//          if(Character.isUpperCase(ch)){
+//              hasUppercase= true;
+//          }
+//        else if(Character.isLowerCase(ch)){
+//              hasLowercase= true;
+//          }
+//          else if(Character.isDigit(ch)){
+//              hasDigit= true;
+//          }
+//          else{
+//              hasSpecialchar =true;
+//          }
+//      }
+//
+//       if(!hasUppercase || !hasLowercase || !hasDigit || !hasSpecialchar){
+//           System.out.println("New password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.");
+//            return;
+//       }
+//       password= newPassword;
+//      System.out.println("Password changed successfully.");
+//
+//    }
+//}
 
 
 
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -1,4 +1,62 @@
+package com.driver;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Workspace extends Gmail{
+
+    private ArrayList<Meeting> calendar; // Stores all the meetings
+
+    public Workspace(String emailId) {
+        // The inboxCapacity is equal to the maximum value an integer can store.
+        super(emailId,Integer.MAX_VALUE);
+        calendar= new ArrayList<>();
+    }
+
+    public void addMeeting(Meeting meeting){
+        //add the meeting to calendar
+        calendar.add(meeting);
+
+    }
+
+    class MyComparator implements Comparator<Meeting> {
+
+        @Override
+        public int compare(Meeting o1, Meeting o2) {
+            String end1=o1.getEndTime().toString().substring(0,2)+o1.getEndTime().toString().substring(3);
+            String end2=o2.getEndTime().toString().substring(0,2)+o2.getEndTime().toString().substring(3);
+
+            return end1.compareTo(end2);
+        }
+    }
+
+    public int findMaxMeetings(){
+        // find the maximum number of meetings you can attend
+        // 1. At a particular time, you can be present in at most one meeting
+        // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
+        // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+        if(calendar.size()==0) return 0;
+        Collections.sort(calendar,new MyComparator());
+
+        int attend=1;
+        String endTime=calendar.get(0).getEndTime().toString().substring(0,2)+calendar.get(0).getEndTime().toString().substring(3);
+
+        for(int i=1;i<calendar.size();i++){
+            String startTime=calendar.get(i).getStartTime().toString().substring(0,2)+calendar.get(i).getStartTime().toString().substring(3);
+
+            //if start TIme of curr meeting > than end time of prev meeting then i can attend this meeting , so attend ++
+            if(startTime.compareTo(endTime)>0){
+                endTime=calendar.get(i).getEndTime().toString().substring(0,2)+calendar.get(i).getEndTime().toString().substring(3);
+                attend++;
+            }
+        }
+        return attend;
+    }
+}
 
 //package com.driver;
 //
@@ -33,58 +91,58 @@
 //
 
 
-
-package com.driver;
-import org.apache.commons.lang3.tuple.Pair;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Collections;
-
-public class Workspace extends Gmail{
-
-
-    private ArrayList<Meeting> calendar; // Stores all the meetings
-
-    public Workspace(String emailId) {
-        super(emailId, Integer.MAX_VALUE); // The inboxCapacity is equal to the maximum value an integer can store.
-        calendar = new ArrayList<>();
-    }
-
-    public void addMeeting(Meeting meeting){
-        calendar.add(meeting); // add the meeting to calendar
-    }
-
-    public int findMaxMeetings(){
-        // find the maximum number of meetings you can attend
-//        // 1. At a particular time, you can be present in at most one meeting
-//        // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
-//        // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
 //
-        int maxMeetings = 0;
-        ArrayList<Pair<LocalTime, Boolean>> timeSlots = new ArrayList<>();
-
-        for(Meeting meeting : calendar) {
-            timeSlots.add(Pair.of(meeting.getStartTime(), true));
-            timeSlots.add(Pair.of(meeting.getEndTime(), false));
-        }
-
-        Collections.sort(timeSlots, (o1, o2) -> o1.getKey().compareTo(o2.getKey()));
-
-        int currentMeetings = 0;
-        for(Pair<LocalTime, Boolean> timeSlot : timeSlots) {
-            if(timeSlot.getValue()) { // A new meeting is starting
-                currentMeetings++;
-                maxMeetings = Math.max(maxMeetings, currentMeetings);
-            } else { // A meeting is ending
-                currentMeetings--;
-            }
-        }
-
-        return maxMeetings;
-    }
-}
-
-
+//package com.driver;
+//import org.apache.commons.lang3.tuple.Pair;
+//import java.time.LocalTime;
+//import java.util.ArrayList;
+//import java.util.Collections;
+//
+//public class Workspace extends Gmail{
+//
+//
+//    private ArrayList<Meeting> calendar; // Stores all the meetings
+//
+//    public Workspace(String emailId) {
+//        super(emailId, Integer.MAX_VALUE); // The inboxCapacity is equal to the maximum value an integer can store.
+//        calendar = new ArrayList<>();
+//    }
+//
+//    public void addMeeting(Meeting meeting){
+//        calendar.add(meeting); // add the meeting to calendar
+//    }
+//
+//    public int findMaxMeetings(){
+//        // find the maximum number of meetings you can attend
+////        // 1. At a particular time, you can be present in at most one meeting
+////        // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
+////        // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+////
+//        int maxMeetings = 0;
+//        ArrayList<Pair<LocalTime, Boolean>> timeSlots = new ArrayList<>();
+//
+//        for(Meeting meeting : calendar) {
+//            timeSlots.add(Pair.of(meeting.getStartTime(), true));
+//            timeSlots.add(Pair.of(meeting.getEndTime(), false));
+//        }
+//
+//        Collections.sort(timeSlots, (o1, o2) -> o1.getKey().compareTo(o2.getKey()));
+//
+//        int currentMeetings = 0;
+//        for(Pair<LocalTime, Boolean> timeSlot : timeSlots) {
+//            if(timeSlot.getValue()) { // A new meeting is starting
+//                currentMeetings++;
+//                maxMeetings = Math.max(maxMeetings, currentMeetings);
+//            } else { // A meeting is ending
+//                currentMeetings--;
+//            }
+//        }
+//
+//        return maxMeetings;
+//    }
+//}
+//
+//
 
 
 
